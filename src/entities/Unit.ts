cbbf03536fc4d0a0077780ec9c7298c0415ab6ef
martin-newbie <g.martin.min@gmail.com>
2024-02-01 22:14:01 +0900

    유닛에 스킬레벨 추가

--- src/entities/Unit.ts
+++ src/entities/Unit.ts
@@ -9,6 +9,7 @@ export class Unit {
     constructor(_idx: number, _defaultLevel: number = 0) {
         this.index = _idx;
         this.level = _defaultLevel;
+        this.skill_level = [this.skill_level_0, this.skill_level_1, this.skill_level_2, this.skill_level_3];
     }
 
     @PrimaryGeneratedColumn()
@@ -29,6 +30,21 @@ export class Unit {
 
     @Column()
     user_uuid: string;
+
+    @Column({default: 0})
+    skill_level_0: number;
+    
+    @Column({default: 0})
+    skill_level_1: number;
+    
+    @Column({default: 0})
+    skill_level_2: number;
+    
+    @Column({default: 0})
+    skill_level_3: number;
+
+    @Required()
+    skill_level: number[];
 
     @ManyToOne(() => User, (user) => user.units, {})
     @JoinColumn({ name: "user_uuid", referencedColumnName: "uuid" })
