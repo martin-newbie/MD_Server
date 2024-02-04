34a91570ce93262772ff253f6f0e26e8b13b3b39
martin-newbie <g.martin.min@gmail.com>
2024-02-04 11:22:55 +0900

    테스트 로그

--- src/services/LoadoutService.ts
+++ src/services/LoadoutService.ts
@@ -24,7 +24,7 @@ export class LoadoutService {
             relations: ['decks']
         });
         if(user == null){
-            return;
+            return; 
         }
 
         const decks = user.decks;
