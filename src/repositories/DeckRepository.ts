dd39f220633cf566337171a3d75838b89f8992f7
martin-newbie <g.martin.min@gmail.com>
2024-02-26 18:28:52 +0900

    불필요한 덱 리포지토리 삭제

--- src/repositories/DeckRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@tsed/di";
-import { Deck } from "../entities/Deck";
-import { Repository } from "typeorm";
-
-
-@Injectable()
-export class DeckRepository extends Repository<Deck>{
-    
-}
