7f29d8e04e678b7f7be35427ecc45461a8447a80
martin-newbie <g.martin.min@gmail.com>
2024-02-26 23:10:06 +0900

    덱 보내줄 때 초기화 안해주는 부분 수정

--- src/controllers/rest/MainMenuController.ts
+++ src/controllers/rest/MainMenuController.ts
@@ -2,6 +2,7 @@ import { Controller, Inject } from "@tsed/di";
 import { BodyParams } from "@tsed/platform-params";
 import { Post } from "@tsed/schema";
 import { UserRepository } from "../../repositories/UserRepository";
+import { Exception } from "@tsed/exceptions";
 
 @Controller("/main-menu")
 export class MainMenuController{
@@ -29,6 +30,12 @@ export class MainMenuController{
         const data: RecieveUserData = JSON.parse(string_data);
 
         const decks = (await this.userRepos.findUserDecks(data.uuid))?.decks;
+        if(decks == null || decks== undefined) throw Exception;
+
+        decks.forEach(deck => {
+            deck.unit_indexes = [deck.unit1, deck.unit2, deck.unit3, deck.unit4, deck.unit5];
+        });
+
         return {
             "decks": decks,
         }
