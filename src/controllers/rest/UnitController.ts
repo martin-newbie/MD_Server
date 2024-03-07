ab8dd2cd46833dd4effb8d5420b18263cabdc2b4
martin-newbie <g.martin.min@gmail.com>
2024-03-07 18:22:22 +0900

    유닛 업그레이드 서비스랑 컨트롤러 연결

--- src/controllers/rest/UnitController.ts
+++ src/controllers/rest/UnitController.ts
@@ -1,21 +1,27 @@
 import { Controller, Inject } from "@tsed/di";
 import { Item } from "../../entities/Item";
 import { QueryParams } from "@tsed/platform-params";
+import { UnitService } from "../../services/UnitService";
 
 @Controller("/unit")
 export class UnitController{
 
+    @Inject()
+    unitService: UnitService;
+
     async upgradeUnitLevel(@QueryParams("input_data") string_data: string){
         const data: RecieveUnitLevelUp = JSON.parse(string_data);
-        
+        await this.unitService.upgradeUnitLevel(data.uuid, data.id, data.use_items, data.use_coin, data.updated_exp);
     }
 
     async upgradeUnitSkill(@QueryParams("input_data") string_data: string) {
         const data: RecieveUnitSkillLevelUp = JSON.parse(string_data);
+        await this.unitService.upgradeUnitSkillLevel(data.uuid, data.id, data.use_items, data.use_coin, data.skill_index);
     }
 
     async upgradeUnitRank(@QueryParams("input_data") string_data: string) {
         const data: RecieveUnitRankUp = JSON.parse(string_data);
+        await this.unitService.upgradeUnitRank(data.uuid, data.id, data.use_items, data.use_coin);
     }
 }
 
@@ -23,7 +29,7 @@ export class RecieveUnitLevelUp {
     uuid: string;
     id: number;
     use_items: Item[];
-    use_bond: number;
+    use_coin: number;
     updated_exp: number;
 }
 
@@ -32,12 +38,12 @@ export class RecieveUnitSkillLevelUp{
     id: number;
     skill_index: number;
     use_items: Item[];
-    use_bond: number;
+    use_coin: number;
 }
 
 export class RecieveUnitRankUp{
     uuid: string;
     id: number;
     use_items: Item[];
-    use_bond: number;
+    use_coin: number;
 }
