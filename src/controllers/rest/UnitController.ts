bcb7b3d0c34612bf61b9dadc4db827253a0eaa90
martin-newbie <g.martin.min@gmail.com>
2024-03-07 18:23:04 +0900

    유닛 업그레이드 3종 라우터 연결

--- src/controllers/rest/UnitController.ts
+++ src/controllers/rest/UnitController.ts
@@ -2,6 +2,7 @@ import { Controller, Inject } from "@tsed/di";
 import { Item } from "../../entities/Item";
 import { QueryParams } from "@tsed/platform-params";
 import { UnitService } from "../../services/UnitService";
+import { Post } from "@tsed/schema";
 
 @Controller("/unit")
 export class UnitController{
@@ -9,16 +10,19 @@ export class UnitController{
     @Inject()
     unitService: UnitService;
 
+    @Post("/upgrade-level")
     async upgradeUnitLevel(@QueryParams("input_data") string_data: string){
         const data: RecieveUnitLevelUp = JSON.parse(string_data);
         await this.unitService.upgradeUnitLevel(data.uuid, data.id, data.use_items, data.use_coin, data.updated_exp);
     }
 
+    @Post("/upgrade-skill")
     async upgradeUnitSkill(@QueryParams("input_data") string_data: string) {
         const data: RecieveUnitSkillLevelUp = JSON.parse(string_data);
         await this.unitService.upgradeUnitSkillLevel(data.uuid, data.id, data.use_items, data.use_coin, data.skill_index);
     }
 
+    @Post("/upgrade-rank")
     async upgradeUnitRank(@QueryParams("input_data") string_data: string) {
         const data: RecieveUnitRankUp = JSON.parse(string_data);
         await this.unitService.upgradeUnitRank(data.uuid, data.id, data.use_items, data.use_coin);
