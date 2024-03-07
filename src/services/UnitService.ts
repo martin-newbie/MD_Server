afda038ed8f9864269b4f185f887b8f2d95a66d4
martin-newbie <g.martin.min@gmail.com>
2024-03-07 18:17:03 +0900

    유닛 스킬레벨 업그레이드

--- src/services/UnitService.ts
+++ src/services/UnitService.ts
@@ -26,4 +26,26 @@ export class UnitService {
         await this.unitRepos.saveUnit(unit);
         await this.userRepos.saveUser(user);
     }
+
+    async upgradeUnitSkillLevel(uuid: string, id: number, use_items: Item[], use_coin: number, skill_index: number) {
+        const user = await this.userRepos.findUserByUUID(uuid);
+        if (!user) throw new Exception(400, "no user available!");
+        const unit = await this.unitRepos.findWithId(id);
+        if (!unit) throw new Exception(400, "no unit available!");
+
+        switch (skill_index) {
+            case 0: unit.skill_level_0++; break;
+            case 1: unit.skill_level_1++; break;
+            case 2: unit.skill_level_2++; break;
+            case 3: unit.skill_level_3++; break;
+        }
+
+        use_items.forEach(item => {
+            user.useItem(item);
+        });
+        user.coin -= use_coin;
+
+        await this.unitRepos.saveUnit(unit);
+        await this.userRepos.saveUser(user);
+    }
 }
